0f566916b69a20157fa678f6a6ebace0e40c5494
merri_23 <mekanhodzaberdiev@gmail.com>
2021-09-07 15:24:20 +0500

    created i18next file

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 import Shimmer from '../Loaders/Shimmer/Shimmer.jsx'
 import Spinner from '../Loaders/Spinner/Spinner.jsx'
 
 const App = () => {
+  const {t} = useTranslation()
+
   return (
     <div className="container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl text-red-900">Menden Gor - marketplace</h1>
+      <h1 className="text-5xl text-red-900">{t('test.mendengor')}</h1>
       <Spinner />
       <Shimmer />
     </div>
