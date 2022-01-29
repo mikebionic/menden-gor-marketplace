d7f67695cc1e6045cbe92bc8d61da62c10207ed5
merri23 <mekanhodzaberdiev@gmail.com>
2022-01-29 13:27:45 +0500

    working on footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Footer: React.FC = () => {
+  return (
+    <div className="w-full h-56">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
