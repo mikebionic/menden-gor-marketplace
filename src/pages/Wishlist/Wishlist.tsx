df6e2c808bdd493a94609c1b872e6bf0212e9ca3
merri23 <mekanhodzaberdiev@gmail.com>
2022-02-22 20:19:33 +0500

    done wishlist table

--- src/pages/Wishlist/Wishlist.tsx
+++ src/pages/Wishlist/Wishlist.tsx
@@ -1,9 +1,106 @@
 import React from 'react';
+import { Table, Form, Space } from 'antd';
+import noimage from 'common/images/noimage.png';
 
-export const Wishlist: React.FC = () => {
-  return (
-    <>
-      <h1>Wishlist</h1>
-    </>
-  );
-};
+const columns = [
+  {
+    title: 'Image',
+    dataIndex: 'image',
+    align: 'center',
+  },
+  {
+    title: 'Description',
+    align: 'center',
+    render: () => (
+      <Space size="small" className="grid grid-rows-3 text-left">
+        <p>Name: CRAFERS - DELUXE MILK CHOCOLATE ( PEACH & VANILLA )</p>
+        <p>Category: CRAFERS</p>
+        <p>Barcode: 4780059600807</p>
+      </Space>
+    ),
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    align: 'center',
+  },
+  {
+    title: 'Action',
+    align: 'center',
+    render: () => (
+      <Space size="middle">
+        <button>Add to Cart</button>
+        <button>wishlist</button>
+      </Space>
+    ),
+  },
+];
+
+const data: any = [];
+for (let i = 1; i <= 10; i++) {
+  data.push({
+    key: i,
+    image: <img src={noimage} alt="" className="w-24 m-auto" />,
+    price: `${i}2 TMT`,
+  });
+}
+
+export class Wishlist extends React.Component {
+  state: any = {
+    bordered: false,
+    loading: false,
+    size: 'default',
+    title: undefined,
+    showHeader: true,
+    footer: false,
+    rowSelection: false,
+    hasData: true,
+    tableLayout: undefined,
+    pagination: false,
+    xScroll: false,
+  };
+
+  handleYScrollChange = (enable: any) => {
+    this.setState({ yScroll: enable });
+  };
+
+  handleXScrollChange = (e: any) => {
+    this.setState({ xScroll: e.target.value });
+  };
+
+  render() {
+    const { xScroll, yScroll, ...state }: any = this.state;
+
+    const scroll: any = {};
+    if (yScroll) {
+      scroll.y = 240;
+    }
+    if (xScroll) {
+      scroll.x = '100vw';
+    }
+
+    const tableColumns: any = columns.map((item) => ({
+      ...item,
+    }));
+    if (xScroll === 'fixed') {
+      tableColumns[0].fixed = true;
+      tableColumns[tableColumns.length - 1].fixed = 'right';
+    }
+
+    return (
+      <>
+        <Form
+          layout="inline"
+          className="shadow-loginShadow components-table-demo-control-bar"
+          style={{ marginBottom: 16 }}
+        ></Form>
+        <Table
+          {...this.state}
+          columns={tableColumns}
+          dataSource={state.hasData ? data : null}
+          scroll={scroll}
+        />
+      </>
+    );
+  }
+}
