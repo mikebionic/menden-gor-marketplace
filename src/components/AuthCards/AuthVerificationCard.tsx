3dc70ee466e427eba5b64d267fcdefcbea692f4a
mike <muhammedjepbarov@gmail.com>
2022-03-31 15:40:20 +0500

    updated auth verification card and services

--- src/components/AuthCards/AuthVerificationCard.tsx
+++ src/components/AuthCards/AuthVerificationCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { IconLabelButton } from 'common/IconLabelButton';
 import { ErrorBoundary } from 'modules/errors';
@@ -8,7 +8,7 @@ import { showToastMessage } from 'sapredux/helpers';
 export const AuthVerificationCard = ({onStageChange, validationData}:any) => {
 
 	const [codeAttempts, set_codeAttempts] = useState(0);
-	const retryTimeoutSeconds = 4
+	const retryTimeoutSeconds = 60
 	const {authMethod, credentials, responseMessage} = validationData
 	const [canRetrySend, set_canRetrySend] = useState(false);
 	const [retryClicked, set_retryClicked] = useState(false)
@@ -41,24 +41,31 @@ export const AuthVerificationCard = ({onStageChange, validationData}:any) => {
 		set_canRetrySend(false)
 	}
 	useEffect(() => {
-		codeAttempts > 3 && onStageChange(1)
+		codeAttempts >= 3 && 
+		showToastMessage({
+			type: 'error',
+			message: 'Attempts exceeded!',
+			position: 'center-top',
+		})
+		&& onStageChange(1)
 	}, [codeAttempts, onStageChange])
 
-	const boxes_list = [
-		{id: 1,	name:'opt1'},
-		{id: 2,	name:'opt2'},
-		{id: 3,	name:'opt3'},
-		{id: 4,	name:'opt4'},
-		{id: 5,	name:'opt5'},
-		{id: 6,	name:'opt6'},
-	]
-	const [verificationCode, setVerificationCode] = useState('')
-	const handleVerificationCodeChange = (e:any) => {
-		(e.target.value.length < 1)
-			? setVerificationCode(verificationCode.substring(0,verificationCode.length-1))
-			: setVerificationCode(`${verificationCode}${e.target.value}`)
+	const [boxes_list, set_boxes_list] = useState([
+		{id: 1,	name:'opt1', value:''},
+		{id: 2,	name:'opt2', value:''},
+		{id: 3,	name:'opt3', value:''},
+		{id: 4,	name:'opt4', value:''},
+		{id: 5,	name:'opt5', value:''},
+		{id: 6,	name:'opt6', value:''},
+	])
 
-		if (e.key === 'Delete' || e.key === 'Backspace' || e.target.value.length < 1) {}
+	const [verificationCode, setVerificationCode] = useState('')
+	const handleVerificationCodeChange = async(e:any, id:number, value?:any) => {
+		const updating_value = e.target.value
+		const updatedList:any = updateValueInList(boxes_list, id, updating_value)
+		await set_boxes_list(updatedList)
+		await setVerificationCode(updatedList.map(({value}:any)=>value).join(''))
+		if (e.key === 'Delete' || e.key === 'Backspace' || updating_value.length < 1) {}
 		else {
       const next = e.target.tabIndex;
       if (next < 6) {
@@ -66,19 +73,59 @@ export const AuthVerificationCard = ({onStageChange, validationData}:any) => {
       }
     }
 	}
-	const handleRemoval = (elmnt: any) => {
-    if (elmnt.key === 'Delete' || elmnt.key === 'Backspace') {
-      const next = elmnt.target.tabIndex - 2;
+
+	const updateValueInList = (arr:any, id:number, value:string) => {
+    const idx = arr.findIndex((item:any) => item.id === id);
+    const item = { ...arr[idx], value: value } ;
+    return [
+      ...arr.slice(0, idx),
+      item,
+      ...arr.slice(idx + 1)
+    ];
+  };
+	const handleRemoval = (e: any) => {
+    if (e.key === 'Delete' || e.key === 'Backspace') {
+      const next = e.target.tabIndex - 2;
       if (next > -1) {
-        elmnt.target.form.elements[next].focus();
+        e.target.form.elements[next].focus();
       }
     }
   }
 
 	const handleSubmit = (e:any) => {
+		const payload = {
+			"verify_code": verificationCode,
+			[authMethod]: credentials,
+		}
+
+		authService.verifyRegister(authMethod, payload).then(
+			(response: any) => {
+				response.status === 1
+					? handleSuccess(response)
+					: showToastMessage({
+							type: 'error',
+							message: response.message,
+							position: 'center-top',
+						});
+			},
+			(error: any) =>
+				showToastMessage({
+					type: 'error',
+					message: error,
+					position: 'center-top',
+				}),
+		);
 		set_codeAttempts(codeAttempts+1)
 		e.preventDefault()
 	}
+	const handleSuccess = (response:any) => {
+    showToastMessage({
+      type: 'success',
+      message: response ? response.message : 'Successfully verified!',
+      position: 'center-top',
+    });
+    onStageChange(3);
+  };
 
 	return (
 		<ErrorBoundary>
@@ -115,7 +162,7 @@ export const AuthVerificationCard = ({onStageChange, validationData}:any) => {
 				<form onSubmit={handleSubmit}>
 					<div className="inline-grid grid-flow-col gap-2 auto-cols-max">
 						{
-							boxes_list.map(({id,name}:any, idx:number) => (
+							boxes_list.map(({id,name,value}:any, idx:number) => (
 								<input
 									key={idx}
 									name={name}
@@ -123,11 +170,12 @@ export const AuthVerificationCard = ({onStageChange, validationData}:any) => {
 									type="text"
 									autoComplete="off"
 									className="w-[50px] h-[50px] bg-fullwhite focus:outline-none focus:ring-0 focus:ring-offset-0 border-[#E6E6E6] text-center focus:ring-offset-transparent focus:ring-transparent focus:border-textColorOrange border border-solid rounded-lg shadow-[0px_0px_4px_rgba(0,0,0,0.25)]"
-									onChange={(e) => handleVerificationCodeChange(e)}
+									onChange={(e) => handleVerificationCodeChange(e, id)}
+									value={value}
 									tabIndex={id}
 									maxLength={1}
-									onFocus={(e) => e.target.value=''}
-									onKeyUp={(e) => handleRemoval(e)}
+									onFocus={(e) => {e.target.value = '';handleVerificationCodeChange(e, id);}}
+									onKeyUp={(e) => {handleRemoval(e)}}
 								/>
 							))
 						}
