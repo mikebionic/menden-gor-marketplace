4c3c231df44a36af193a792f034f2dbb5ca623e0
merri23 <mekanhodzaberdiev@gmail.com>
2022-04-14 16:00:17 +0500

    for test

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
-export const Divider = ({ title = 'Just for you' }: any) => {
-  return (
-    <div className="grid gap-4 my-4 grid-cols-[max-content_1fr] grid-rows-[2rem]">
-      <h1 className="text-2xl font-medium text-center text-black uppercase dark:text-darkTextWhiteColor">
-        {title}
-      </h1>
-      <span className="w-full h-0 mt-3 border-2 border-[#DEDFE4] dark:border-darkTextWhiteColor"></span>
-    </div>
-  );
+interface IPropsDivider { 
+	title?: string;
+	mobile?: boolean;
+}
+
+export const Divider:React.FC<IPropsDivider> = ({ title = 'Just for you', mobile }) => {
+	return (
+			mobile ? (
+			<div className="grid gap-4 my-4 grid-cols-[max-content_1fr] grid-rows-[2rem]">
+				<h1 className="text-2xl font-medium text-center text-black uppercase dark:text-darkTextWhiteColor">
+					{title}
+				</h1>
+				<span className="w-full h-0 mt-3 border-2 border-[#DEDFE4] dark:border-darkTextWhiteColor"></span>
+			</div >
+			) : (
+				<div className="grid gap-4 my-4 grid-cols-[max-content_1fr] grid-rows-[2rem]">
+				<h1 className="text-2xl font-medium text-center text-black uppercase dark:text-darkTextWhiteColor">
+					{title}
+				</h1>
+				<span className="w-full h-0 mt-3 border-2 border-[#DEDFE4] dark:border-darkTextWhiteColor"></span>
+			</div>
+			)
+	);
 };
