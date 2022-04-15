adb528cc91bb7b2f2303b1cc8040ff86507e26be
mike <muhammedjepbarov@gmail.com>
2022-04-15 18:45:19 +0500

    updated payment methdo and checkout page

--- src/components/Payment/PaymentMethods.tsx
+++ src/components/Payment/PaymentMethods.tsx
@@ -4,8 +4,10 @@ import { otherService } from 'sapredux/services'
 import { online_payment_methods } from 'sapredux/services/mock_data/payment.mock'
 import { serviceConfig } from 'configs'
 import { Image } from 'common/Image'
+import { getCurrentCurrency } from 'sapredux/helpers'
 
 export const PaymentMethods = ({ id, onChange }: any) => {
+	const currency_code = getCurrentCurrency().name
 	const [data, set_data] = useState([])
 	useEffect(() => {
 		otherService.fetch_payment_methods().then((response: any) => {
@@ -16,37 +18,40 @@ export const PaymentMethods = ({ id, onChange }: any) => {
 
 	return (
 		<ErrorBoundary>
-			{data.map(({ id, name, description }: any, idx: number) => (
-				<div
-					className="grid grid-flow-col gap-0 cursor-pointer grid-rows-OrderLine grid-cols-OrderLine"
-					key={idx}
-					onClick={() => {
-						set_current_method_id(id)
-						onChange(id)
-					}}
-				>
-					<div className="row-span-3 p-4 m-auto">
-						<input
-							className="w-3 h-3 my-auto transform scale-125 cursor-pointer text-firstColorGradientFromDark dark:text-darkFirstColor focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-transparent focus:ring-transparent border-textColorOrange dark:border-darkFirstColor"
-							type="radio"
-							name={`method-${name}`}
-							checked={id === current_method_id ? true : false}
+			{data.map(
+				({ id, name, description }: any, idx: number) =>
+					!(currency_code === 'USD' && id === 2) && (
+						<div
+							className="grid grid-flow-col gap-0 cursor-pointer grid-rows-OrderLine grid-cols-OrderLine"
+							key={idx}
 							onClick={() => {
 								set_current_method_id(id)
 								onChange(id)
 							}}
-						/>
-					</div>
-					<div className="col-span-2 mx-0 my-auto">
-						<h3 className="text-sm font-semibold font-oxygen dark:text-darkTextWhiteColor">
-							{name}
-						</h3>
-					</div>
-					<div className="col-span-2 row-span-2 font-oxygen dark:text-darkText">
-						{description}
-					</div>
-				</div>
-			))}
+						>
+							<div className="row-span-3 p-4 m-auto">
+								<input
+									className="w-3 h-3 my-auto transform scale-125 cursor-pointer text-firstColorGradientFromDark dark:text-darkFirstColor focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-transparent focus:ring-transparent border-textColorOrange dark:border-darkFirstColor"
+									type="radio"
+									name={`method-${name}`}
+									checked={id === current_method_id ? true : false}
+									onClick={() => {
+										set_current_method_id(id)
+										onChange(id)
+									}}
+								/>
+							</div>
+							<div className="col-span-2 mx-0 my-auto">
+								<h3 className="text-sm font-semibold font-oxygen dark:text-darkTextWhiteColor">
+									{name}
+								</h3>
+							</div>
+							<div className="col-span-2 row-span-2 font-oxygen dark:text-darkText">
+								{description}
+							</div>
+						</div>
+					),
+			)}
 		</ErrorBoundary>
 	)
 }
