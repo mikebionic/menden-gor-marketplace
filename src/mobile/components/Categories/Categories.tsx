6cd7b4d06fae571bc2f20bf92a708c21a7c9221f
merri23 <mekanhodzaberdiev@gmail.com>
2022-04-24 21:21:49 +0500

    working on categories page

--- src/mobile/components/Categories/Categories.tsx
+++ src/mobile/components/Categories/Categories.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Image } from 'common/Image'
 
 export const Categories: React.FC = () => {
 	return (
-		<div>
-			<div></div>
+		<div className="mt-2 grid grid-cols-[1fr_auto]">
+			<div className="grid grid-cols-2 gap-2">
+				<div className="w-24 h-24 bg-fullwhite shadow-defaultShadow">
+					<Image src="" />
+				</div>
+				<div className="w-24 h-24 bg-fullwhite shadow-defaultShadow">
+					<Image src="" />
+				</div>
+				<div className="w-24 h-24 bg-fullwhite shadow-defaultShadow">
+					<Image src="" />
+				</div>
+			</div>
+			<div>Hello</div>
 		</div>
 	)
 }
