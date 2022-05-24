8c1cc4852a305ab5a5156c1ec0f0145f7cb1a65e
mike <muhammedjepbarov@gmail.com>
2022-05-24 15:42:25 +0500

    sliders with filepathm and product name length limited

--- src/components/Carousel/CarouselSlider.tsx
+++ src/components/Carousel/CarouselSlider.tsx
@@ -1,38 +1,37 @@
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/splide/dist/css/splide.min.css';
-import { Image } from 'common/Image';
-
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/splide/dist/css/splide.min.css'
+import { Image } from 'common/Image'
 
 export const CarouselSlider = ({ images }: any) => {
-  return (
-    <Splide
-      className={`mb-8`}
-      options={{
-        type: 'loop',
-        gap: '1rem',
-        autoplay: true,
-        pauseOnHover: true,
-        resetProgress: false,
-        lazyLoad: true,
-        speed: 700,
-        arrows: 'slider',
-        height: '25rem',
-      }}
-      hasSliderWrapper
-      hasAutoplayProgress
-    >
-      {images.map((image: any, idx: number) => (
-        <SplideSlide
-          className="h-full p-[0_0_56.23%_0] grid place-content-center place-items-center"
-          key={idx}
-        >
-          <Image
-            src={image.filePathM}
-            alt={image.title}
-            className="absolute w-full h-auto"
-          />
-        </SplideSlide>
-      ))}
-    </Splide>
-  );
-};
+	return (
+		<Splide
+			className={`mb-8`}
+			options={{
+				type: 'loop',
+				gap: '1rem',
+				autoplay: true,
+				pauseOnHover: true,
+				resetProgress: false,
+				lazyLoad: true,
+				speed: 700,
+				arrows: 'slider',
+				height: '25rem',
+			}}
+			hasSliderWrapper
+			hasAutoplayProgress
+		>
+			{images.map((image: any, idx: number) => (
+				<SplideSlide
+					className="h-full p-[0_0_56.23%_0] grid place-content-center place-items-center"
+					key={idx}
+				>
+					<Image
+						src={window.innerWidth < 768 ? image.filePathM : image.filePathR}
+						alt={image.title}
+						className="absolute w-full h-auto"
+					/>
+				</SplideSlide>
+			))}
+		</Splide>
+	)
+}
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,114 +1,114 @@
-import { Link } from 'react-router-dom';
-import { Badge } from 'antd';
+import { Link } from 'react-router-dom'
+import { Badge } from 'antd'
 
-import { Image } from 'common/Image';
-import { ProductAddToCart } from 'components/ProductCard';
-import { Ribbon } from 'common/Ribbon';
-import { ErrorBoundary } from 'modules/errors';
-import { routeConstants } from 'navigation';
-import { StarRate } from 'common/StarRate';
-import { PriceButton } from 'common/PriceButton';
-import { WishlistButton } from 'common/WishlistButton';
+import { Image } from 'common/Image'
+import { ProductAddToCart } from 'components/ProductCard'
+import { Ribbon } from 'common/Ribbon'
+import { ErrorBoundary } from 'modules/errors'
+import { routeConstants } from 'navigation'
+import { StarRate } from 'common/StarRate'
+import { PriceButton } from 'common/PriceButton'
+import { WishlistButton } from 'common/WishlistButton'
 
 const ProductCard = ({ data }: any) => {
-  const {
-    id,
-    name,
-    description,
-    realPrice,
-    priceValue,
-    currencySymbol,
-    image,
-    isNew,
-    discount,
-    wishlist,
-    ratingValue,
-    categoryName,
-  } = data;
+	const {
+		id,
+		name,
+		description,
+		realPrice,
+		priceValue,
+		currencySymbol,
+		image,
+		isNew,
+		discount,
+		wishlist,
+		ratingValue,
+		categoryName,
+	} = data
 
-  let resource_description = description
-    ? description.length > 50
-      ? `${description.slice(0, 50)}...`
-      : description
-    : '';
+	let resource_description = description
+		? description.length > 50
+			? `${description.slice(0, 50)}...`
+			: description
+		: ''
 
-  let resource_name = name
-    ? name.length > 25
-      ? `${name.slice(0, 50)}...`
-      : name
-    : '';
+	let resource_name = name
+		? name.length > 25
+			? `${name.slice(0, 25)}...`
+			: name
+		: ''
 
-  return (
-    <ErrorBoundary>
-      <div className="relative grid items-center xl:w-64 md:w-52 md:h-96 grid-cols-1 mt-4 bg-white dark:bg-darkComponentColor rounded grid-rows-[max-content_30px_auto_auto_auto] xl:h-[26rem] shadow-defaultShadow">
-        <Badge.Ribbon
-          text={discount}
-          placement="start"
-          className={`cursor-default xl:top-[82%] md:top-[79%] discount-left ${
-            discount ?? 'hidden'
-          }`}
-          color="red"
-        >
-          <div className="items-center justify-center mx-auto my-3 overflow-hidden bg-gray-200 dark:bg-darkBgColor xl:w-56 xl:h-64 md:w-44 md:h-52 ">
-            <div className="relative">
-              {isNew ? <Ribbon /> : null}
-              <span className="absolute top-0 right-0">
-                <WishlistButton resId={id} wishlist={wishlist} />
-              </span>
-              <span className="absolute right-0 xl:top-52 md:top-40">
-                <ProductAddToCart resourceId={id} />
-              </span>
-            </div>
+	return (
+		<ErrorBoundary>
+			<div className="relative grid items-center xl:w-64 md:w-52 md:h-96 grid-cols-1 mt-4 bg-white dark:bg-darkComponentColor rounded grid-rows-[max-content_30px_auto_auto_auto] xl:h-[26rem] shadow-defaultShadow">
+				<Badge.Ribbon
+					text={discount}
+					placement="start"
+					className={`cursor-default xl:top-[82%] md:top-[79%] discount-left ${
+						discount ?? 'hidden'
+					}`}
+					color="red"
+				>
+					<div className="items-center justify-center mx-auto my-3 overflow-hidden bg-gray-200 dark:bg-darkBgColor xl:w-56 xl:h-64 md:w-44 md:h-52 ">
+						<div className="relative">
+							{isNew ? <Ribbon /> : null}
+							<span className="absolute top-0 right-0">
+								<WishlistButton resId={id} wishlist={wishlist} />
+							</span>
+							<span className="absolute right-0 xl:top-52 md:top-40">
+								<ProductAddToCart resourceId={id} />
+							</span>
+						</div>
 
-            <Link to={`${routeConstants.product.route}${id}/${name}`}>
-              <Image
-                src={image}
-                alt={`${name} - ${resource_description}`}
-                className="object-contain object-center w-full h-full max-w-full max-h-full py-4 lg:w-full lg:h-full"
-              />
-            </Link>
-          </div>
-        </Badge.Ribbon>
-        <Link to={`${routeConstants.product.route}${id}/${name}`}>
-          <div className="mx-auto my-0 text-center w-max">
-            <h3 className="mx-auto my-0 font-semibold text-black dark:text-darkTextWhiteColor">
-              {resource_name}
-            </h3>
-            <hr className="w-full dark:border-darkFirstColor" />
-          </div>
-        </Link>
+						<Link to={`${routeConstants.product.route}${id}/${name}`}>
+							<Image
+								src={image}
+								alt={`${name} - ${resource_description}`}
+								className="object-contain object-center w-full h-full max-w-full max-h-full py-4 lg:w-full lg:h-full"
+							/>
+						</Link>
+					</div>
+				</Badge.Ribbon>
+				<Link to={`${routeConstants.product.route}${id}/${name}`}>
+					<div className="mx-auto my-0 text-center w-max">
+						<h3 className="mx-auto my-0 font-semibold text-black dark:text-darkTextWhiteColor">
+							{resource_name}
+						</h3>
+						<hr className="w-full dark:border-darkFirstColor" />
+					</div>
+				</Link>
 
-        <p className="mx-4 text-sm text-justify text-gray-400 dark:text-darkText">
-          {categoryName}
-        </p>
-        <StarRate disabled={true} value={ratingValue} />
-        <div
-          className={`flex items-center py-1 border-t border-gray-300 dark:border-darkFirstColor ${
-            discount ? 'justify-between' : 'justify-end'
-          }`}
-        >
-          {discount && (
-            <p className="mx-4 text-sm text-justify text-gray-400 line-through">
-              {realPrice} {currencySymbol}
-            </p>
-          )}
+				<p className="mx-4 text-sm text-justify text-gray-400 dark:text-darkText">
+					{categoryName}
+				</p>
+				<StarRate disabled={true} value={ratingValue} />
+				<div
+					className={`flex items-center py-1 border-t border-gray-300 dark:border-darkFirstColor ${
+						discount ? 'justify-between' : 'justify-end'
+					}`}
+				>
+					{discount && (
+						<p className="mx-4 text-sm text-justify text-gray-400 line-through">
+							{realPrice} {currencySymbol}
+						</p>
+					)}
 
-          <p className="mx-4 text-sm text-justify">
-            <PriceButton
-              priceValue={priceValue}
-              currencySymbol={currencySymbol}
-              width="w-auto"
-              coloredButton={true}
-            />
-          </p>
-        </div>
-        {/* <p className="mx-4 mb-2 text-sm text-justify">{resource_description}</p> */}
-        {/* <h3 className="w-auto px-3 mx-auto my-0 rounded-full bg-gradient-to-r from-firstColorGradientFromDark to-secondColorGradientToLight">
+					<p className="mx-4 text-sm text-justify">
+						<PriceButton
+							priceValue={priceValue}
+							currencySymbol={currencySymbol}
+							width="w-auto"
+							coloredButton={true}
+						/>
+					</p>
+				</div>
+				{/* <p className="mx-4 mb-2 text-sm text-justify">{resource_description}</p> */}
+				{/* <h3 className="w-auto px-3 mx-auto my-0 rounded-full bg-gradient-to-r from-firstColorGradientFromDark to-secondColorGradientToLight">
           {priceValue} {currencySymbol} */}
-        {/* </h3> */}
-      </div>
-    </ErrorBoundary>
-  );
-};
+				{/* </h3> */}
+			</div>
+		</ErrorBoundary>
+	)
+}
 
-export default ProductCard;
+export default ProductCard
