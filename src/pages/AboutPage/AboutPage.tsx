03bf76c5fa411435d57e5ee473d9378e57a285e3
merri23 <mekanhodzaberdiev@gmail.com>
2022-09-29 14:42:31 +0500

    removed img about

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -7,11 +7,11 @@ export const AboutPage: React.FC = () => {
 	return (
 		<div className="w-full px-0 md:py-16">
 			<div className="grid items-center justify-around w-full gap-4 mx-auto my-8 lg:grid-flow-col lg:auto-cols-auto md:grid-flow-row md:auto-rows-max">
-				<img
+				{/* <img
 					className="h-auto col-start-1 col-end-2 mx-auto my-0 gap-x-2 w-96"
 					src="https://cdn.pixabay.com/photo/2021/02/14/11/41/monkeys-6014204_1280.jpg"
 					alt=""
-				/>
+				/> */}
 				<div className="w-auto py-0 mx-auto my-0 font-serif md:px-8">
 					<h1 className="mb-5 font-bold text-black capitalize dark:text-darkTextWhiteColor min-phone:text-3xl min-phone:text-center md:text-7xl">
 						About us
